b2ffd077fd417150f477ea857f0d5e05361e5a23
Elior Abramoviz Ducker <elior@fb.com>
2022-02-22 23:44:39 +0200

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
-import './App.css';
+import data from './data.json'
+import './App.scss';
 
 function App() {
+  const {congratulations, mainPicture, mainText} = data;
+  console.log('congratulations.length % 3', congratulations.length % 3);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">אודי בן 55</div>
+      <div className="main-picture-container">
+        <div className="main-text">{mainText}</div>
+        <div className="main-picture" style={{backgroundImage: `url(${mainPicture})`}}></div>
+      </div>
+      <div className="list-container">
+        {congratulations.map(item => {
+          return (
+            <div className={`item-container ${item.size ? `size-${item.size}` : ''}`} style={{backgroundImage: `url(${item.imageUrl})`}}>
+              {item.writer}
+            </div>
+          );
+        })}
+{/*
+        {congratulations.length % 3 === 2 ?
+          <div className="placeholder"></div> :
+          null
+        }
+
+        {congratulations.length % 3 === 1 ?
+          <>
+            <div className="placeholder"></div>
+            <div className="placeholder"></div>
+          </> :
+          null
+        } */}
+      </div>
     </div>
   );
 }
