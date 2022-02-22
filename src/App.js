3a5f7179ee21c9c97bd88602bd035de766a22346
Elior Abramoviz Ducker <elior@fb.com>
2022-02-23 00:48:30 +0200

    Add support for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import {useState, useEffect} from 'react';
+
 import logo from './logo.svg';
 import data from './data.json'
 import './App.scss';
 
+let scrollY = 0;
 function App() {
   const {congratulations, mainPicture, mainText} = data;
-  console.log('congratulations.length % 3', congratulations.length % 3);
+  const [page, setPage] = useState('app');
+  const [selectedCongrats, setSelectedCongrats] = useState(null);
+
+  const createHandleItemClick = (congrats) => {
+    return () => {
+      setPage('congrats-modal');
+      setSelectedCongrats(congrats);
+    }
+  }
+
   return (
-    <div className="App">
+    <div className={`App ${page !== 'app' ? 'disabled' : ''}`}>
       <div className="header">אודי בן 55</div>
       <div className="main-picture-container">
         <div className="main-text">{mainText}</div>
-        <div className="main-picture" style={{backgroundImage: `url(${mainPicture})`}}></div>
+        <div className="main-picture border-with-shadow" style={{backgroundImage: `url(${mainPicture})`}}></div>
       </div>
       <div className="list-container">
-        {congratulations.map(item => {
+        {congratulations.map((item, index) => {
           return (
-            <div className={`item-container ${item.size ? `size-${item.size}` : ''}`} style={{backgroundImage: `url(${item.imageUrl})`}}>
+            <div key={index} onClick={createHandleItemClick(item)} className={`item-container border-with-shadow ${item.size ? `size-${item.size}` : ''}`} style={{backgroundImage: `url(${item.imageUrl})`}}>
               {item.writer}
             </div>
           );
         })}
+      </div>
+
+      <div className={`congrats-modal ${page === 'congrats-modal' ? 'visible' : ''}`}>
+        <div className="close-button" onClick={() => setPage('app')}>x</div>
+        {selectedCongrats && (
+          <>
+            <div className="hide-content-from-scroll"></div>
+            <div className="writer-header border-with-shadow" style={{backgroundImage: `url(${selectedCongrats.imageUrl})`}}>
+              {`בהמון אהבה, מ${selectedCongrats.writer}`}
+            </div>
+            {selectedCongrats.title && (
+              <div className="title">
+                {selectedCongrats.title}
+              </div>
+            )}
+            {selectedCongrats.content && (
+              <div className="content">
+                {selectedCongrats.content}
+              </div>
+            )}
+            {/* <div className="hide-content-from-scroll bottom"></div> */}
+          </>)
+        }
       </div>
     </div>
   );
